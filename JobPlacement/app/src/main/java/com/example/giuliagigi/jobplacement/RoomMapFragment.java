c7c7a626793c903a26b60da4326240a34846f050
MarcoEsposito90 <expolife90@gmail.com>
2015-11-06 17:02:51 +0100

    mappa: movimento e scalatura

--- JobPlacement/app/src/main/java/com/example/giuliagigi/jobplacement/RoomMapFragment.java
+++ JobPlacement/app/src/main/java/com/example/giuliagigi/jobplacement/RoomMapFragment.java
@@ -5,7 +5,10 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,14 +27,22 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 
-public class RoomMapFragment extends Fragment implements GLSurfaceView.Renderer {
+public class RoomMapFragment extends Fragment implements GLSurfaceView.Renderer, View.OnTouchListener, ScaleGestureDetector.OnScaleGestureListener, GestureDetector.OnGestureListener {
+
+    private static final float MOVE_SCALE_FACTOR = 0.05f;
+    private static final float SCALE_SCALE_FACTOR = 0.005f;
 
     private Room room;
     private GLSurfaceView surfaceView;
+    private ScaleGestureDetector scaleGestureDetector;
+    private GestureDetector gestureDetector;
 
     /* these two object must be manipulated only in the background thread */
     private World world;
     private FrameBuffer frameBuffer;
+    private Object3D mapPlane;
+    float lastX,lastY, lastSpan;
+    boolean isScaling = false;
 
     //////////////////////////////////////////////////////////////////////////////////////
     // --------------------- CONSTRUCTOR -----------------------------------------------//
@@ -66,6 +77,9 @@ public class RoomMapFragment extends Fragment implements GLSurfaceView.Renderer 
         // we must set the version og the GL engine we are using
         surfaceView.setEGLContextClientVersion(2);
         surfaceView.setRenderer(this);
+        surfaceView.setOnTouchListener(this);
+        scaleGestureDetector = new ScaleGestureDetector(getActivity().getApplicationContext(),this);
+        gestureDetector = new GestureDetector(getActivity().getApplicationContext(), this);
         return surfaceView;
     }
 
@@ -100,7 +114,7 @@ public class RoomMapFragment extends Fragment implements GLSurfaceView.Renderer 
 
         world = new World();
 
-        Object3D plane = Primitives.getPlane(1,3.0f);
+        mapPlane = Primitives.getPlane(1,3.0f);
         InputStream in = null;
 
         try {
@@ -116,12 +130,12 @@ public class RoomMapFragment extends Fragment implements GLSurfaceView.Renderer 
         else            mapTexture = new Texture(1024,1024, RGBColor.RED);
 
         TextureManager.getInstance().addTexture("mapTexture", mapTexture);
-        plane.setTexture("mapTexture");
-        plane.translate(5,0,10);
-        plane.scale(10);
+        mapPlane.setTexture("mapTexture");
+        mapPlane.translate(2.5f, 0, 10);
+        mapPlane.scale(10);
 
         world.setAmbientLight(255,255,255);
-        world.addObject(plane);
+        world.addObject(mapPlane);
     }
 
     @Override
@@ -139,5 +153,167 @@ public class RoomMapFragment extends Fragment implements GLSurfaceView.Renderer 
         world.draw(frameBuffer);        // draws a 2D image of the world on the frame buffer
 
         frameBuffer.display();          // displays the current content of the frame buffer
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    // --------------------- USER INTERACTION METHODS ----------------------------------//
+    //////////////////////////////////////////////////////////////////////////////////////
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+//        switch (event.getAction()){
+//
+//            case MotionEvent.ACTION_DOWN:
+//
+//                lastX = event.getX();
+//                lastY= event.getY();
+//
+//                return true;
+//
+//            case MotionEvent.ACTION_MOVE:
+//
+//                final float dx = event.getX() - lastX;
+//                final float dy = event.getY() - lastY;
+//
+//                lastX = event.getX();
+//                lastY = event.getY();
+//
+//                /* we cannot manipulate 3D objects in the UI thread, so we enqueue a runnable
+//                * for the surface view*/
+//                surfaceView.queueEvent(new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                        mapPlane.translate(dx*0.01f,dy*0.01f,0);
+//                    }
+//                });
+//
+//                return true;
+//
+//            default: return true;
+//        }
+
+        scaleGestureDetector.onTouchEvent(event);
+        gestureDetector.onTouchEvent(event);
+
+        return true;
+    }
+
+
+    /* ------------- scaling --------------------------------------- */
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+
+        final float ds = detector.getCurrentSpan() - lastSpan;
+        final float dx = detector.getFocusX() - lastX;
+        final float dy = detector.getFocusY() - lastY;
+
+        lastSpan = detector.getCurrentSpan();
+        lastX = detector.getFocusX();
+        lastY = detector.getFocusY();
+
+        surfaceView.queueEvent(new Runnable() {
+            @Override
+            public void run() {
+
+                float newScale = 1 + ds*SCALE_SCALE_FACTOR;
+                mapPlane.scale(newScale);
+                mapPlane.translate(dx*MOVE_SCALE_FACTOR,dy*MOVE_SCALE_FACTOR,0);
+            }
+        });
+
+        return false;
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+
+        lastSpan = detector.getCurrentSpan();
+        lastX = detector.getFocusX();
+        lastY = detector.getFocusY();
+        isScaling = true;
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+
+        isScaling = false;
+    }
+
+
+    /* ------------- moving --------------------------------------- */
+    @Override
+    public boolean onDown(MotionEvent e) {
+
+        if(!isScaling){
+
+            lastX = e.getX();
+            lastY = e.getY();
+        }
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+        if(!isScaling){
+
+
+        }
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+
+        if(!isScaling){
+
+
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+        if(!isScaling){
+
+            final float dx = e2.getX() - lastX;
+            final float dy = e2.getY() - lastY;
+
+            lastX = e2.getX();
+            lastY = e2.getY();
+
+            surfaceView.queueEvent(new Runnable() {
+                @Override
+                public void run() {
+
+                    mapPlane.translate(dx*MOVE_SCALE_FACTOR,dy*MOVE_SCALE_FACTOR,0);
+                }
+            });
+        }
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+        if(!isScaling){
+
+
+        }
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+        if(!isScaling){
+
+
+        }
+        return false;
     }
 }
