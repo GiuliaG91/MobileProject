e3b7591e2de3b2641b93de7dd2e69f0935087ec2
MarcoEsposito90 <expolife90@gmail.com>
2015-11-18 16:52:07 +0100

    uyd

--- JobPlacement/app/src/main/java/com/example/giuliagigi/jobplacement/CompanyStudentSearchFragment.java
+++ JobPlacement/app/src/main/java/com/example/giuliagigi/jobplacement/CompanyStudentSearchFragment.java
@@ -30,7 +30,6 @@ public class CompanyStudentSearchFragment extends Fragment {
     private Integer position=0;
     private GlobalData globalData;
 
-    private OnFragmentInteractionListener mListener;
 
 
     public static CompanyStudentSearchFragment newInstance()
@@ -84,13 +83,6 @@ public class CompanyStudentSearchFragment extends Fragment {
     }
 
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onCreateOptionsMenu(
             Menu menu, MenuInflater inflater) {
@@ -113,34 +105,14 @@ public class CompanyStudentSearchFragment extends Fragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
+
     public void addFiters(List<Tag> tag_list, List<String> degree_list, List<String> field_list) {
         adapter.setFactory(tag_list, degree_list, field_list);
         adapter.setAdapter();
